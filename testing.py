7c99a875d67c726dac69525def106a47d6c7cb8e
kartik <kartik.sharma10@infosys.com>
2020-10-17 10:53:27 +0530

    email integration

--- testing.py
+++ testing.py
@@ -1,5 +1,7 @@
 import zomatoAPI
 import pandas as pd
+import sendMail
+pd.set_option('display.max_colwidth', -1)
 
 def test(loc, cuisine, price):
     config={ "user_key":"a67c96823657beee597b9234eed8906d"}
@@ -18,26 +20,60 @@ def test(loc, cuisine, price):
         results+=zomato.restaurant_search("", lat, lng, str(cuisines_dict.get(cuisine)), start=i)
         
     df = pd.DataFrame(results)
-
-    def budget_group(row):
-        if row['budget'] <300 :
-            return 'lesser than 300'
-        elif 300 <= row['budget'] <700 :
-            return 'between 300 to 700'
-        else:
-            return 'more than 700'
-
-    df['budget'] = df.apply(lambda row: budget_group (row), axis=1)
     
-    restaurant_df = df[(df.budget == price)]
-    restaurant_df = restaurant_df.sort_values(['rating'], ascending=False)    
+    return df
 
-    return restaurant_df
 
+def budget_group(row):
+    if row['budget'] <300 :
+        return 'lesser than 300'
+    elif 300 <= row['budget'] <700 :
+        return 'between 300 to 700'
+    else:
+        return 'more than 700'
+    
     
 loc = "delhi"
 cuisine = 'chinese'
 price='lesser than 300'  
+mailid='kartik10messi@gmail.com'
 res1=test(loc, cuisine, price)
+res1=res1.sort_values(['rating'], ascending=False).reset_index()
 
-#{"code":403,"status":"Forbidden","message":"Invalid API Key"}
+res_filter = res1.copy()
+res_filter['budget'] = res_filter.apply(lambda row: budget_group (row), axis=1)
+
+restaurant_df = res_filter[(res_filter.budget == price)]
+restaurant_df = restaurant_df.sort_values(['rating'], ascending=False)
+
+contentDf = res1.loc[:10,['name','address','budget','rating']]
+contentDf = contentDf.rename(columns={'name':'Restraunt Name','address':'Address',
+                             'budget':'Average budget for two', 'rating':'Average Rating'})
+
+mail_content=contentDf.to_html(index=False, justify='center')
+#
+#start = '''<!DOCTYPE html>
+#<html lang="en">
+#<head>
+#  <title>Bootstrap Example</title>
+#  <meta charset="utf-8">
+#  <meta name="viewport" content="width=device-width, initial-scale=1">
+#  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+#</head>
+#<body>
+#<div class="container">
+#  <h2>Top 10 restraunts -</h2><br>
+#  <table class="table table-hover" border="1">
+#'''
+#
+#end='''
+#</div>
+#</body>
+#</html>
+#'''
+#mail_content = mail_content.replace('<table border="1" class="dataframe">','')
+#mail_content= start+mail_content+end
+mail =sendMail.Mail(mailid, mail_content)
+mail.send()
+
+#classes='table table-striped',
